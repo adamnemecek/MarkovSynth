dfeea4b02c3a76121b7b22dddb0949e3e0592fc7
alayers2 <alayers2@gmail.com>
2014-04-10 19:04:25 -0400

    Initial Commit

---
+++ MarkovChain/AYAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AYAAppDelegate.h
+//  MarkovChain
+//
+//  Created by Andrew Ayers on 4/10/14.
+//  Copyright (c) 2014 AyersAudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AYAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MarkovChain/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MarkovChain
+//
+//  Created by Andrew Ayers on 4/10/14.
+//  Copyright (c) 2014 AyersAudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AYAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AYAAppDelegate class]));
+    }
+}
